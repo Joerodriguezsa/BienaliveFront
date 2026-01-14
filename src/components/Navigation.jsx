e99074d8265f92fa88f43d1c4d035d95c73d0c17
John Rodriguez Santana <johned32@hotmail.com>
2026-01-13 21:39:20 -0500

    Update Navigation.jsx

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,14 +1,15 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
 function Navigation() {
-    return (
-        <ul>
-            <li>
-                <Link to="/">Home 
-                {/* <i className="fa-solid fa-angle-down"></i> */}
-                </Link>
-                {/* <ul className="sub-menu">
+  return (
+    <ul>
+      <li>
+        <Link to="/">
+          Home
+          {/* <i className="fa-solid fa-angle-down"></i> */}
+        </Link>
+        {/* <ul className="sub-menu">
                     <li><Link to="/">Home one</Link></li>
                     <li><Link to="/index-2">Home two</Link></li>
                     <li><Link to="/index-3">Home three</Link></li>
@@ -37,53 +38,72 @@ function Navigation() {
                         </ul>
                     </li>
                 </ul> */}
-            </li>
-            {/* <li><Link to="/page-about">About</Link></li> */}
-            <li>
-                <Link to="/page-team">Team </Link>
-                {/* <ul className="sub-menu"> */}
-                    {/* <li><Link to="/page-gallery">Gallery</Link></li>
+      </li>
+      {/* <li><Link to="/page-about">About</Link></li> */}
+      <li>
+        <Link to="/page-team">Team </Link>
+        {/* <ul className="sub-menu"> */}
+        {/* <li><Link to="/page-gallery">Gallery</Link></li>
                     <li>
                         <Link className="arrow" to="#0">Team</Link>
                         <ul className="sub-sub-menu">
                             <li><Link to="/page-team">Team</Link></li> */}
-                            {/* <li><Link to="/page-team-details">Team Details</Link></li> */}
-                        {/* </ul>
+        {/* <li><Link to="/page-team-details">Team Details</Link></li> */}
+        {/* </ul>
                     </li> */}
-                    
-                    {/* <li><Link to="/page-testimonial">Testimonials</Link></li>
+
+        {/* <li><Link to="/page-testimonial">Testimonials</Link></li>
                     <li><Link to="/page-faq">FAQ’s</Link></li>
                     <li><Link to="/page-404">404 Error</Link></li> */}
-                {/* </ul> */}
-            </li>
-            <li>
-                <Link to="/page-services">Services </Link>
-                {/* <ul className="sub-menu">
+        {/* </ul> */}
+      </li>
+      <li>
+        <Link to="/page-services">Services </Link>
+        {/* <ul className="sub-menu">
                     <li><Link to="/page-services">Service Grid</Link></li>
                     <li><Link to="/page-service-details">Service Details</Link></li>
                 </ul> */}
-            </li>
-            <li>
-                <Link to="/shop-products">Products <i className="fa-solid fa-angle-down"></i></Link>
-                <ul className="sub-menu">
-                    <li><Link to="/shop-products">Shop Product</Link></li>
-                    <li><Link to="/shop-products-sidebar">Products Sidebar</Link></li>
-                    <li><Link to="/shop-product-details">Product Details</Link></li>
-                    <li><Link to="/shop-checkout">Checkout</Link></li>
-                    <li><Link to="/shop-cart">Cart</Link></li>
-                </ul>
-            </li>
-            {/* <li>
+      </li>
+      <li>
+        <Link to="/shop-products">
+          Products <i className="fa-solid fa-angle-down"></i>
+        </Link>
+        <ul className="sub-menu">
+          <li>
+            <Link to="/shop-products">Shop Product</Link>
+          </li>
+          <li>
+            <Link to="/shop-products-sidebar">Products Sidebar</Link>
+          </li>
+          <li>
+            <Link to="/shop-product-details">Product Details</Link>
+          </li>
+          <li>
+            <Link to="/shop-checkout">Checkout</Link>
+          </li>
+          <li>
+            <Link to="/shop-cart">Cart</Link>
+          </li>
+        </ul>
+      </li>
+      {/* <li>
                 <Link to="/#0">News <i className="fa-solid fa-angle-down"></i></Link>
                 <ul className="sub-menu">
                     <li><Link to="/news-grid">News</Link></li>
                     <li><Link to="/news-details">News Details</Link></li>
                 </ul>
             </li> */}
-            <li><Link to="/page-contact">Contact</Link></li>
-            <li><Link to="/page-users">Users</Link></li>
-        </ul>
-    );
+      <li>
+        <Link to="/page-contact">Contact</Link>
+      </li>
+      <li>
+        <Link to="/page-users">Users</Link>
+      </li>
+      <li>
+        <Link to="/page-team-members">Team Members</Link>
+      </li>
+    </ul>
+  );
 }
 
 export default Navigation;
